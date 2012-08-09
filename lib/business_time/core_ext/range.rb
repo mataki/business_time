87472468b15a992e118baac08bee864a94037e5d
Akihiro Matsumura <matsumura.aki@gmail.com>
2012-08-09 10:49:57 +0900

    Add options to range for skip_holiday

--- lib/business_time/core_ext/range.rb
+++ lib/business_time/core_ext/range.rb
@@ -1,7 +1,11 @@
 class Range
-  def business_days(time = Time.now)
+  def business_days(options = {})
+    time = options[:time].nil? ? Time.now : options[:time]
     first_day = self.first.is_a?(Date) ? self.first : self.first.business_day.since(time).to_date
     last_day = self.last.is_a?(Date) ? self.last : self.last.business_day.since(time).to_date
-    (first_day..last_day).select{ |day| day.workday? }
+    days = (first_day..last_day)
+    skip_holiday = options[:skip_holiday].nil? ? true : options[:skip_holiday]
+    days = days.select{ |day| day.workday? } if skip_holiday
+    days
   end
 end
--- test/test_range_extensions.rb
+++ test/test_range_extensions.rb
@@ -3,7 +3,7 @@ require 'helper'
 class TestRangeExtensions < Test::Unit::TestCase
   should "respond to business day list from fixnum range" do
     now = Time.parse("April 9th, 2010, 12:33 pm")
-    list = (3..8).business_days(now)
+    list = (3..8).business_days(time: now)
     assert_equal list.first, 3.business_day.since(now).to_date
     assert_equal list.last, 8.business_day.since(now).to_date
     assert_equal list.size, (3..8).count
@@ -11,9 +11,17 @@ class TestRangeExtensions < Test::Unit::TestCase
 
   should "respond to business day list from date range" do
     now = Time.parse("April 9th, 2010, 12:33 pm")
-    list = (3.business_day.since(now).to_date..8.business_day.since(now).to_date).business_days(now)
+    list = (3.business_day.since(now).to_date..8.business_day.since(now).to_date).business_days(time: now)
     assert_equal list.first, 3.business_day.since(now).to_date
     assert_equal list.last, 8.business_day.since(now).to_date
-    assert_equal list.count, (3..8).count
+    assert_equal list.count, 6
+  end
+
+  should "respond to business day list without skip_holiday" do
+    now = Time.parse("April 9th, 2010, 12:33 pm")
+    list = (3.business_day.since(now).to_date..8.business_day.since(now).to_date).business_days(time: now, skip_holiday: false)
+    assert_equal list.first, 3.business_day.since(now).to_date
+    assert_equal list.last, 8.business_day.since(now).to_date
+    assert_equal list.count, 8
   end
 end
